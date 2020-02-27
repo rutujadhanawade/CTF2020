6a8965269ce01755e99358916834234069349790
ADWAIT <adwaitnsk.2009@rediffmail.com>
2020-02-27 23:37:36 +0530

    made some changes

--- lakshya/ctf/views.py
+++ lakshya/ctf/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import login
 from django.http import HttpResponse
 from .models import UserProfile
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth
 
 
 def index(request):
@@ -24,10 +24,11 @@ def signup(request):
         user = User.objects.create_user(username=username, password=password)
         userprofile = UserProfile(user=user, email=email, phone=phone, clg=clg, dept=dept, firstname=firstname,
                                           lastname=lastname, year=year)
-        userprofile.save()
-        login(request, user)
+        user.save();
+        userprofile.save();
+        print("user succesfully created")
 
-        return render(request, 'ctf/first.html', context={'user': user})
+        return redirect('login')
 
     elif request.method == 'GET':
         return render(request, 'ctf/signup.html')
@@ -41,7 +42,7 @@ def login(request):
 
             if user is not None:
                 auth.login(request, user)
-                return redirect("ctf/first.html")
+                return redirect("/")
             else:
                 messages.info(request, 'invalid credentials')
                 return redirect('ctf/login.html')
