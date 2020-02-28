2300135f2c1273e593815ad9f10669d513b32a0b
RutujaD182 <dhanawaderutuja1002@gmail.com>
2020-02-29 04:59:01 +0530

    reverted

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('sign', views.signup, name='signup'),
-    path('login', views.login1, name='login1')
-]
